3c2a47943a319224c4bf5c87adaebfc9c2e6ef44
Noorku <noorskoulayb@gmail.com>
2021-10-24 20:59:10 -0700

    Adding click event and Change color according to className

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// set initial count
+let count = 0;
+// Select value and button
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+btns.forEach(function (btn) {
+btn.addEventListener('click', function(e) {
+ const styles =  e.currentTarget.classList
+ if(styles.contains('decrease')) {
+     count--;
+ }
+ else if(styles.contains("increase")) {
+     count++;
+ }
+ else {
+     count = 0;
+ }
+ if(count > 0) {
+     value.style.color = "green";
+ }else if(count < 0) {
+     value.style.color = "orange";
+ }else {
+     value.style.color = "black";
+ }
+   value.textContent = count; 
+})
+})
